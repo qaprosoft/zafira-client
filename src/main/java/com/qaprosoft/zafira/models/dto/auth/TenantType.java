ba8099aa839f8bb082ad1fe7e32925f52e20c291
tsvrko <tsvrko@gmail.com>
2019-07-11 19:01:42 +0300

    lombok annotations are added for auth dto

--- src/main/java/com/qaprosoft/zafira/models/dto/auth/TenantType.java
+++ src/main/java/com/qaprosoft/zafira/models/dto/auth/TenantType.java
@@ -17,9 +17,15 @@ package com.qaprosoft.zafira.models.dto.auth;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @JsonInclude(Include.NON_NULL)
 public class TenantType implements Serializable {
     private static final long serialVersionUID = 8220711984153406216L;
@@ -28,9 +34,6 @@ public class TenantType implements Serializable {
     private String serviceUrl;
     private boolean multitenant;
     private boolean useArtifactsProxy;
-
-    public TenantType() {
-    }
 
     public TenantType(String tenant) {
         this.tenant = tenant;
@@ -42,35 +45,4 @@ public class TenantType implements Serializable {
         this.useArtifactsProxy = useArtifactsProxy;
     }
 
-    public String getTenant() {
-        return tenant;
-    }
-
-    public void setTenant(String tenant) {
-        this.tenant = tenant;
-    }
-
-    public String getServiceUrl() {
-        return serviceUrl;
-    }
-
-    public void setServiceUrl(String serviceUrl) {
-        this.serviceUrl = serviceUrl;
-    }
-
-    public boolean isMultitenant() {
-        return multitenant;
-    }
-
-    public void setMultitenant(boolean multitenant) {
-        this.multitenant = multitenant;
-    }
-
-    public boolean isUseArtifactsProxy() {
-        return useArtifactsProxy;
-    }
-
-    public void setUseArtifactsProxy(boolean useArtifactsProxy) {
-        this.useArtifactsProxy = useArtifactsProxy;
-    }
 }
